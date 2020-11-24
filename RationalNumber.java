04c0d91178aa834c17e4cf3bc8cb32ff6c62f2d1
Kirsten Szeto <kszeto20@stuy.edu>
2020-11-24 02:45:46 -0500

    added up to reduce

--- RationalNumber.java
+++ RationalNumber.java
@@ -35,4 +35,36 @@ public class RationalNumber extends RealNumber {
     RationalNumber recip = new RationalNumber(numerator, denominator);
     return recip;
   }
+
+  public boolean equals(RationalNumber other) {
+    if (this.getNumerator() == other.getNumerator()) {
+      if (this.getDenominator() == other.getDenominator()) {
+        return true;
+      }
+    }
+      return false;
+  }
+
+  public String toString() {
+    return "" + numerator + "/" + denominator;
+  }
+
+  public static int gcd(int a, int b) {
+    int gcd = 0;
+
+    for(int i = 1; i <= a && i <= b; i++) {
+      if (a%i==0 && b%i==0) {
+        gcd = i;
+      }
+    }
+    return gcd;
+  }
+
+  public void reduce() {
+    int gcdVal = gcd(numerator, denominator);
+    numerator = numerator / gcdVal;
+    denominator = denominator / gcdVal;
+  }
+
+
 }
