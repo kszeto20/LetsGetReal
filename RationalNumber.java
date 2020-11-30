37a86f2704f7721cbc1a5c5e8a4fbe10c286fd61
Kirsten Szeto <kszeto20@stuy.edu>
2020-11-29 22:47:17 -0500

    toString() changed to account for special cases

--- RationalNumber.java
+++ RationalNumber.java
@@ -52,7 +52,15 @@ public class RationalNumber extends Number {
   }
 
   public String toString() {
-    return "" + numerator + "/" + denominator;
+    if (denominator == 1) {
+      return (numerator + "");
+    }
+    else if (numerator == 0) {
+      return "0";
+    }
+    else {
+      return ("" + numerator + "/" + denominator);
+    }
   }
 
   public static int gcd(int a, int b) {
